41dc0bf1f3a903a013328cbfe00f9bdb887450cd
avivgatya <101098311+avivgatya@users.noreply.github.com>
2022-04-03 23:43:20 +0300

    3_4

---
+++ 16.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+namespace a
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("61");
+
+            for (int exponent = 5; exponent <= 1000; exponent++)
+                UpdateString(sb);
+
+            Console.WriteLine("sum of digits of 2^1000 is:"+SumDigits(sb));
+        }
+        public static void UpdateString(StringBuilder sb)
+        {
+            int num, carry = 0;
+            for (int i = 0; i < sb.Length; i++)
+            {
+                num = (sb[i] - '0') * 2;
+                num += carry;
+                carry = 0;
+                if (num < 10) sb[i] = (char)(num + '0');
+                else
+                {
+                    carry = num / 10;
+                    sb[i] = (char)((num % 10) + '0');
+                }
+
+            }
+            if (carry > 0) sb.Append((char)(carry + '0'));
+        }
+        public static int SumDigits(StringBuilder sb)
+        {
+            int sum = 0;
+            for(int i=0;i<sb.Length;i++)
+                sum += sb[i] - '0';
+            return sum;
+        }
+    }
+}
